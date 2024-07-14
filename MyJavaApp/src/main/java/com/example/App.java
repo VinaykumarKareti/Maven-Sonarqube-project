cbcff93e2119a87218657d91ec40969c96d4bfa9
Kareti Venkata Vinay Kumar <21pa1a0574@vishnu.edu.in>
2024-07-14 13:31:02 +0530

    adding files

--- MyJavaApp/src/main/java/com/example/App.java
+++ MyJavaApp/src/main/java/com/example/App.java
@@ -2,11 +2,16 @@ package com.example;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-
     public int add(int a, int b) {
         return a + b;
+    }
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    public int div(int a, int b) {
+        return a / b;
     }
 }
--- MyJavaApp/src/test/java/com/example/AppTest.java
+++ MyJavaApp/src/test/java/com/example/AppTest.java
@@ -10,5 +10,11 @@ public class AppTest {
         App app = new App();
         int result = app.add(3, 4);
         assertEquals(7, result);
+        int result1 = app.sub(30, 4);
+        assertEquals(26, result1);
+        int result2 = app.mul(3, 4);
+        assertEquals(12, result2);
+        int result3 = app.div(16, 4);
+        assertEquals(4, result3);
     }
 }
